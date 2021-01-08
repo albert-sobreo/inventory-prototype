aa4f71d1c458464c0077a4a5b2955a0c699f0fa9
MichaelAngeloMalicsi <michaelmalicsi090398@gmail.com>
2021-01-08 20:20:58 +0800

    logs update

--- app/views/logs.py
+++ app/views/logs.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render, redirect
-from app.models import Warehouse, Product, User, Vendor, Customer, Purchase_Item, Purchase_Order, Sales_Item, Sales_Order, Transfer, Transfer_Item, Spoilage, Spoilage_Item
+from app.models import User, Purchase_Order, Sales_Order, Transfer, Spoilage
 
 def purchase_page(request):
     if request.session.is_empty():
         return redirect('/login/')
     context = {
-        'items': Product.objects.all().order_by('name'),
-        'warehouses': Warehouse.objects.all(),
+        'purchases': Purchase_Order.objects.all(),
         'me': User.objects.select_related().get(login__username=request.session.get('username')),
-        'ref_id': new_ref_id,
-        'vendors': Vendor.objects.all(),
     }
     return render(request, 'purchase_log.html', context)
 
@@ -17,10 +14,8 @@ def sales_page(request):
     if request.session.is_empty():
         return redirect('/login/')
     context = {
-        'items': Product.objects.all().order_by('name'),
+        'sales': Sales_Order.objects.all(),
         'me': User.objects.select_related().get(login__username=request.session.get('username')),
-        'new_ref_id': new_ref_id,
-        'customers': Customer.objects.all(),
     }
     return render(request, 'sales_log.html', context)
 
@@ -28,16 +23,17 @@ def transfer_page(request):
     if request.session.is_empty():
         return redirect('/login/')
     context = {
-        'items': Product.objects.all().order_by('name'),
+        'transfers': Transfer.objects.all(),
         'me': User.objects.select_related().get(login__username=request.session.get('username')),
-        'new_ref_id': new_ref_id,
-        'customers': Customer.objects.all(),
-        'warehouses': Warehouse.objects.all(),
     }
     return render(request, 'transfer_log.html', context)
 
 def spoilage_page(request):
     if request.session.is_empty():
         return redirect('/login/')
+    context = {
+        'spoils': Spoilage.objects.all(),
+        'me': User.objects.select_related().get(login__username=request.session.get('username')),
+    }
     return render(request, 'spoilage_logs.html', context)
     
