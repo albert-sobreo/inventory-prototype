951b36b037d9b50a2bb4b5b935e0a7255b1992ee
Jan Albert Sobreo <janalbertsobreo@icloud.com>
2021-01-10 00:30:40 +0800

    updates

--- app/views/receipts.py
+++ app/views/receipts.py
@@ -14,11 +14,7 @@ def sales_approved(request):
     if request.session.is_empty():
         return redirect('/login/')
     context = {
-<<<<<<< HEAD
-        'purchases': Purchase_Order.objects.filter(approved=True),
-=======
         'purchases': Sales_Order.objects.filter(approved=True),
->>>>>>> 31f192c6973e2a944eb288799d570518def03fd9
         'me': User.objects.select_related().get(login__username=request.session.get('username')),
     }
     return render(request, 'sales_approved.html', context)
@@ -27,11 +23,7 @@ def purchase_notapproved(request):
     if request.session.is_empty():
         return redirect('/login/')
     context = {
-<<<<<<< HEAD
-        'sales': Sales_Order.objects.filter(approved=False),
-=======
         'sales': Purchase_Order.objects.filter(approved=False),
->>>>>>> 31f192c6973e2a944eb288799d570518def03fd9
         'me': User.objects.select_related().get(login__username=request.session.get('username')),
     }
     return render(request, 'purchase_not.html', context)
@@ -40,11 +32,7 @@ def purchase_approved(request):
     if request.session.is_empty():
         return redirect('/login/')
     context = {
-<<<<<<< HEAD
-        'sales': Sales_Order.objects.filter(approved=True),
-=======
         'sales': Purchase_Order.objects.filter(approved=True),
->>>>>>> 31f192c6973e2a944eb288799d570518def03fd9
         'me': User.objects.select_related().get(login__username=request.session.get('username')),
     }
     return render(request, 'purchase_approved.html', context)
